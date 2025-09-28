5f0b270906fdecb1c0d44b78659033a569a42cb2
Farhan-Khan7 <fk5518744@gmail.com>
2025-09-28 15:00:24 +0530

    I have done sighup page

--- script.js
+++ script.js
@@ -118,42 +118,164 @@ const donationsLink = document.querySelector(".donatitions");
 const jobLink = document.querySelector(".job");
 const mentorshipLink = document.querySelector(".mentorship");
 const signupLink = document.querySelector(".signup");
+
 const homeMain = document.querySelector(".homeMain")
+const alumniMain = document.querySelector(".alumniMain")
+const signupMain = document.querySelector(".signupMain")
+
+const alumniContainer = document.querySelector(".alumni-container")
+const studentContainer = document.querySelector(".student-container")
+const instituteContainer = document.querySelector(".institue-container")
+const loginContainer = document.querySelector(".login-container")
+
+const footer = document.querySelector(".site-footer")
 
 
-homeLink.addEventListener("click" , function(){
-    homeMain.style.display = "initial"
+homeLink.addEventListener("click", function () {
+    homeMain.style.display = "block"
+    footer.style.display = "block"
+    signupMain.style.display = "none"
+    alumniMain.style.display = "none"
+    signupLink.style.color = "#E0E0E0"
+    homeLink.style.color = "#ffd60a"
+
+
+
 })
 
 
-alumniLink.addEventListener("click" , function(){
+alumniLink.addEventListener("click", function () {
     homeMain.style.display = "none"
+    signupMain.style.display = "none"
+    alumniMain.style.display = "block"
+    footer.style.display = "block"
 })
 
 
-eventsLink.addEventListener("click" , function(){
+eventsLink.addEventListener("click", function () {
     homeMain.style.display = "none"
+    signupMain.style.display = "none"
+    alumniMain.style.display = "none"
 })
 
 
-donationsLink.addEventListener("click" , function(){
+donationsLink.addEventListener("click", function () {
     homeMain.style.display = "none"
+    signupMain.style.display = "none"
+    alumniMain.style.display = "none"
 })
 
 
-jobLink.addEventListener("click" , function(){
+jobLink.addEventListener("click", function () {
     homeMain.style.display = "none"
+    signupMain.style.display = "none"
+    alumniMain.style.display = "none"
 })
 
 
-mentorshipLink.addEventListener("click" , function(){
+mentorshipLink.addEventListener("click", function () {
     homeMain.style.display = "none"
+    signupMain.style.display = "none"
+    alumniMain.style.display = "none"
 })
 
 
-signupLink.addEventListener("click" , function(){
+signupLink.addEventListener("click", function () {
+    homeLink.style.color = "#E0E0E0"
+    signupLink.style.color = "#ffd60a"
     homeMain.style.display = "none"
+    alumniMain.style.display = "none"
+    footer.style.display = "none"
+    signupMain.style.display = "initial"
 })
 
+
+
+
+const buttons = document.querySelectorAll('.form-changes-btn button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active')); // pehle sab remove
+        button.classList.add('active'); // clicked button ko add
+    });
+});
+
+
+
+
+// Sirf Alumni button Students button Institute button
+const alumniBtn = document.querySelector('#alumni');
+const studentsBtn = document.querySelector('#students');
+const instituteBtn = document.querySelector('#institute');
+
+studentsBtn.addEventListener("click", function () {
+    studentContainer.style.display = "block"
+    alumniContainer.style.display = "none"
+    instituteContainer.style.display = "none"
+})
+
+alumniBtn.addEventListener("click", function () {
+    alumniContainer.style.display = "block"
+    studentContainer.style.display = "none"
+    instituteContainer.style.display = "none"
+
+})
+
+instituteBtn.addEventListener("click", function () {
+    instituteContainer.style.display = "block"
+    alumniContainer.style.display = "none"
+    studentContainer.style.display = "none"
+
+
+})
+
+
+// const loginBtnInstitute = document.querySelector('#Login-btn-institute');
+const loginBtnAlumni = document.querySelector('#Login-btn-alumni');
+const loginBtnstudent = document.querySelector('#Login-btn-student');
+const loginBtnInstitute = document.querySelector('#Login-btn-institute');
+const signupBtnAlumni = document.querySelector('#Signup-btn-alumni');
+const signupBtnStudent = document.querySelector('#Signup-btn-student');
+const signupBtnInstitute = document.querySelector('#Signup-btn-institute');
+
+const alumniLogin = document.querySelector("#alumni-login")
+const studentLogin = document.querySelector("#student-login")
+const instituteLogin = document.querySelector("#institute-login")
+// ----- LOGIN FROM EACH LOGIN -----
+
+
+// ============== ALUMNI LOGIN SIGNUP PAGE =================
+signupBtnAlumni.addEventListener("click", function(){
+    alumniLogin.style.display = "none"
+    alumniContainer.style.display = "block"
+})
+loginBtnAlumni.addEventListener("click", function(){
+    alumniLogin.style.display = "block"
+    alumniContainer.style.display = "none"
+})
+
+// ============== STUDENT LOGIN SIGNUP PAGE =================
+signupBtnStudent.addEventListener("click", function(){
+    studentLogin.style.display = "none"
+    studentContainer.style.display = "block"
+})
+loginBtnstudent.addEventListener("click", function(){
+    studentLogin.style.display = "block"
+    studentContainer.style.display = "none"
+
+})
+
+
+// ==============  INSTITUTE LOGIN SIGNUP PAGE =================
+
+signupBtnInstitute.addEventListener("click", function(){
+    instituteLogin.style.display = "none"
+    instituteContainer.style.display = "block"
+})
+loginBtnInstitute.addEventListener("click", function(){
+    instituteLogin.style.display = "block"
+    instituteContainer.style.display = "none"
+})
 
 
